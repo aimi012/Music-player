b47e20822c278b90cb2696a0e66ca7688d2a1f3f
aimi012 <aimiamirul01@gmail.com>
2022-12-29 14:23:14 +0800

    completed

--- app.js
+++ app.js
@@ -16,14 +16,54 @@ function createSongLIst() {
   return list;
 }
 
+const player = document.getElementById("player");
+
 const songList = document.getElementById("songList");
 songList.appendChild(createSongLIst());
 
-songList.onclick = function (e) {
+const links = document.querySelectorAll('li');
+for (const link of links) {
+    link.addEventListener('click', setSong)
+}
+
+function setSong (e) {
+  const hp = document.querySelector("#headphone");
+  hp.classList.remove('pulse')
+
   const source = document.getElementById("source");
   source.src = "Music/" + e.target.innerText;
 
-  const player = document.getElementById("player");
+  document.querySelector(
+    "#currentSong"
+  ).innerHTML = `Now Playing + ${e.target.innerText}`;
+
   player.load();
   player.play();
+  hp.classList.add("pulse");
 };
+
+function playAudio() {
+  if (player.readyState) {
+    player.play();
+  }
+}
+
+function pauseAudio() {
+  player.pause();
+}
+
+const slider = document.getElementById("volumeSlider");
+slider.oninput = function (e) {
+  const volume = e.target.value;
+  player.volume = volume;
+};
+
+function updateProgress() {
+  if (player.currentTime > 0) {
+    const progressBar = document.getElementById("progress");
+    progressBar.value = (player.currentTime / player.duration) * 100;
+  }
+
+  console.log(player.currentTime);
+  console.log(player.duration);
+}
